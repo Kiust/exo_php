a4b6fc154a646dfaa69654da9ed88b5becfcbd3e
Luca <lucattparte@gmail.com>
2022-02-25 16:07:21 +0100

    exo7.php completed & verified

--- exo7.php
+++ exo7.php
@@ -1,18 +1,34 @@
 <h1>Exercise 7</h1>
 
 <p>Ecrire un algorithme permettant de renvoyer la catégorie d’un enfant en fonction de son âge :
-Poussin : entre 6 et 7 ans
-Pupille : entre 8 et 9 ans
-Minime : entre 10 et 11 ans
-Cadet : à partir de 12 ans
-Si la catégorie n’est pas gérée, merci de le préciser.</p>
+    Poussin : entre 6 et 7 ans
+    Pupille : entre 8 et 9 ans
+    Minime : entre 10 et 11 ans
+    Cadet : à partir de 12 ans
+    Si la catégorie n’est pas gérée, merci de le préciser.</p>
+    
+    <h2>Résultat</h2>
+    
+    <?php
 
-<h2>Résultat</h2>
+$age = 7;
 
-<?php
+if ($age >= 12) {
+    $categorie = "Cadet";
+} elseif ($age >= 10) {
+    $categorie = "Minime";
+} elseif ($age >= 8) {
+    $categorie = "Pupille";
+} elseif ($age >= 6) {
+    $categorie = "Pupille";
+    
+} else {
+    echo "Veuillez saisir un âge";
+}
+echo "La personne qui a $age ans appartient à la catégorie : $categorie";
 
-$age = 6;
 
+/*  
 if ($age >= 12) {
     $categorie = "Cadet";
 } elseif ($age >= 10 && $age <= 11) {
@@ -26,9 +42,10 @@ if ($age >= 12) {
     echo "Veuillez saisir un âge";
 }
 echo "La personne qui a $age ans appartient à la catégorie : $categorie";
+
+*/
+
     
-
-
 
 /*
 
@@ -36,5 +53,7 @@ Affichage :
 L’enfant qui a 10 ans appartient à la catégorie « Minime »
 
 */
+
+
 
 ?>
